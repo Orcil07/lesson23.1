97769e2758f18a1b13834186fcf23edae6d39564
Orcil07 <orcil9675@gmail.com>
2024-10-30 23:25:59 +0300

    Add files via upload

---
+++ lesson23.1.py
@@ -0,0 +1,66 @@
+class House:
+    def __init__(self, name, number_of_floors, ):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+            return self
+
+    def __iadd__(self, other: int):
+        return self.__add__(other)
+
+    def __radd__(self, other: int):
+        return self.__add__(other)
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+
+h1 = House("hightower", 10)
+h2 = House("warehouse", 5)
+
+print(h1)
+print(h2)
+print(h1 == h2)
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h1)
